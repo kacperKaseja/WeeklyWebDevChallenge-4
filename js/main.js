5f901188611f8fb8a75b031f91986427e5f6acfb
Kacper Kaseja <yamakaszi10@o2.pl>
2020-02-03 18:19:54 +0100

    Adda SLider Images to carousele

--- js/main.js
+++ js/main.js
@@ -11,8 +11,38 @@ hamburger.addEventListener('click', hamburgerClick);
 const searchInput = document.querySelector('.header__search-input');
 const searchIco = document.querySelector('.header__search-ico path')
 
-// searchInput.addEventListener('focus', ()=>{
-//     searchIco.style.fill = "#56CCC8";
-// })
+searchInput.addEventListener('focus', () => {
+    searchIco.style.fill = "#56CCC8";
+})
 
-// searchInput.focus()
+searchInput.addEventListener('blur', () => {
+    searchIco.style.fill = "red";
+})
+
+
+//SLider Section
+
+const btnPrevius = document.querySelector('.slider__button--prev');
+const btnNext = document.querySelector('.slider__button--next');
+
+
+const sliderContainer = document.querySelector('.slider');
+const images = ['1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg'
+]
+
+let i = images.length;
+console.log(i);
+
+btnPrevius.addEventListener('click', () => {
+    i = (i < images.length + 1 && i > 1) ? (i = i - 1) : (i = images.length);
+    sliderContainer.style.background = `url('${images[i-1]}') no-repeat center`;
+    sliderContainer.backgroundSize = 'cover';
+});
+btnNext.addEventListener('click', () => {
+    i = (i < images.length) ? (i = i + 1) : (i = 1);
+    sliderContainer.style.background = `url('${images[i-1]}') no-repeat center`;
+    sliderContainer.backgroundSize = 'cover';
+});
